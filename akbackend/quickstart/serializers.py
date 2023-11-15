7d96616f1b4bb2709a9b6bf0c177f561a3d236bf
Nabilla Shafira Bachtiar <nabillashafirab@gmail.com>
2023-11-15 17:58:07 +0700

    error on ClassName

---
+++ akbackend/quickstart/serializers.py
@@ -0,0 +1,21 @@
+# app_name/serializers.py
+from rest_framework import serializers
+from .models import ClassName, Tag, Operator
+
+class ClassNameSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ClassName
+        fields = '__all__'
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = '__all__'
+
+class OperatorSerializer(serializers.ModelSerializer):
+    class_name = ClassNameSerializer()
+    tags = TagSerializer(many=True)
+
+    class Meta:
+        model = Operator
+        fields = '__all__'
--- akbackend/quickstart/views.py
+++ akbackend/quickstart/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+from rest_framework import permissions
+from akbackend.quickstart.serializers import ClassNameSerializer, TagSerializer, OperatorSerializer
+
+
+class ClassNameViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = ClassName.objects.all()
+    serializer_class = ClassNameSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class TagViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class OperatorViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Operator.objects.all()
+    serializer_class = OperatorSerializer
+    permission_classes = [permissions.IsAuthenticated]
