c9b3d57f13d6c89c8fe0a78b9e04d21821893ad2
Nabilla Shafira Bachtiar <nabillashafirab@gmail.com>
2023-11-15 16:16:47 +0700

    defining model

--- akbackend/quickstart/models.py
+++ akbackend/quickstart/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+class ClassName(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Operator(models.Model):
+    RARITY_CHOICES = [
+        (1, '1-Star'),
+        (2, '2-Star'),
+        (3, '3-Star'),
+        (4, '4-Star'),
+        (5, '5-Star'),
+        (6, '6-Star'),
+    ]
+
+    name = models.CharField(max_length=255)
+    rarity = models.IntegerField(choices=RARITY_CHOICES)
+    class_name = models.ForeignKey(ClassName, on_delete=models.CASCADE)
+    # image = models.ImageField(upload_to='operator_images/')
+    tags = models.ManyToManyField(Tag, related_name='operators')
+
+    def __str__(self):
+        return self.name
