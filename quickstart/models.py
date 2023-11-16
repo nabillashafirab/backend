f66f38b32a0ab286fc2c772d7afad2fbac1e5c47
Ihsan M. A <ihsan.saktia@gmail.com>
2023-11-16 18:06:35 +0700

    Add unique name for Tags

--- quickstart/models.py
+++ quickstart/models.py
@@ -7,7 +7,7 @@ class ClassName(models.Model):
         return self.name
 
 class Tag(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
 
     def __str__(self):
         return self.name
