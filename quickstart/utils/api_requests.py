c7e362fc596cb1012d6d4f7536ee4c3b977bdcbf
Nabilla Shafira Bachtiar <nabillashafirab@gmail.com>
2023-11-16 20:56:33 +0700

    api request

--- quickstart/utils/api_requests.py
+++ quickstart/utils/api_requests.py
@@ -2,14 +2,11 @@ import requests
 
 # Define new data to create
 new_data = {
-    "name": "Firewatch",
-    "rarity": 5,
-    "class_name": "Sniper",
-    "tags": ["Nuker", "DPS", "Ranged"],
+    "name": "Support",
 }
 
 # The API endpoint to communicate with
-url_post = "http://127.0.0.1:8000/operators/"
+url_post = "http://127.0.0.1:8000/tags/"
 
 # A POST request to tthe API
 post_response = requests.post(url_post, json=new_data)
