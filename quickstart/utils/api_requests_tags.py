416846e4843f6d3ceb9492c0fb5870aca24b0538
Nabilla Shafira Bachtiar <nabillashafirab@gmail.com>
2023-11-20 15:35:03 +0700

    installed pipenv + added api_requests

---
+++ quickstart/utils/api_requests_tags.py
@@ -0,0 +1,16 @@
+import requests
+
+# Define new data to create
+new_data = {
+    "name": "Support",
+}
+
+# The API endpoint to communicate with
+url_post = "http://127.0.0.1:8000/tags/"
+
+# A POST request to tthe API
+post_response = requests.post(url_post, json=new_data)
+
+# Print the response
+post_response_json = post_response.json()
+print(post_response_json)
