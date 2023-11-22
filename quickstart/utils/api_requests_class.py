faefe7733590de884ff6ae2a878e25036a83ad9e
Nabilla Shafira Bachtiar <nabillashafirab@gmail.com>
2023-11-22 11:02:05 +0700

    GET and POST classes data

--- quickstart/utils/api_requests_class.py
+++ quickstart/utils/api_requests_class.py
@@ -1,16 +1,41 @@
 import requests
 
-# Define new data to create
-new_data = {
-    "name": "Guard",
-}
-
 # The API endpoint to communicate with
 url_post = "http://127.0.0.1:8000/classes/"
 
-# A POST request to tthe API
-post_response = requests.post(url_post, json=new_data)
+try:
+    # Make a GET request
+    r = requests.get('https://aceship.github.io/AN-EN-Tags/json/tl-type.json')
+    r.raise_for_status()  # Raise an error for bad responses (4xx or 5xx)
+    
+    # Decode JSON content
+    response = r.json()
+    
+    # Print the response from the GET request
+    print(response)
 
-# Print the response
-post_response_json = post_response.json()
-print(post_response_json)
+    # Post data
+    for i in response:
+        new_data = {
+            "name": i["type_en"]
+        }
+        
+        # A POST request to the API
+        post_response = requests.post(url_post, json=new_data)
+        
+        # Print the response from the POST request
+        post_response.raise_for_status()  # Raise an error for bad responses (4xx or 5xx)
+        post_response_json = post_response.json()
+        print(post_response_json)
+
+except requests.exceptions.HTTPError as errh:
+    print("HTTP Error:", errh)
+except requests.exceptions.ConnectionError as errc:
+    print("Error Connecting:", errc)
+except requests.exceptions.Timeout as errt:
+    print("Timeout Error:", errt)
+except requests.exceptions.RequestException as err:
+    print("Something went wrong:", err)
+except requests.exceptions.JSONDecodeError as json_err:
+    print("JSON Decode Error:", json_err)
+    print("Response content:", r.text)  # Print the actual content for further inspection
--- quickstart/utils/api_requests_tags.py
+++ quickstart/utils/api_requests_tags.py
@@ -1,6 +1,11 @@
 import requests
 
-# Define new data to create
+# GET request
+r = requests.get('https://aceship.github.io/AN-EN-Tags/json/tl-type.json')
+response = r.json()
+d = dict()
+
+# Get tags name and put into new dictionary
 new_data = {
     "name": "Survival",
 }
@@ -8,7 +13,7 @@ new_data = {
 # The API endpoint to communicate with
 url_post = "http://127.0.0.1:8000/tags/"
 
-# A POST request to tthe API
+# A POST request to the API
 post_response = requests.post(url_post, json=new_data)
 
 # Print the response
